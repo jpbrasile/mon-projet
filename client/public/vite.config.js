a98f9483b2f5c88f1b5c3ae5cd237e361ed0c6a7
brasile <8331027+jpbrasile@users.noreply.github.com>
2025-02-21 17:01:21 +0100

    vite looks at the right place now

--- client/public/vite.config.js
+++ client/public/vite.config.js
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  root: 'client/public',
+  plugins: [react()],
+  build: {
+    outDir: '../dist', // This will create the build output in client/dist
+    emptyOutDir: true,
+  }
 });
