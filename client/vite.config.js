2a3701d890858fe71aa84a0a9c87f96d37b319ee
brasile <8331027+jpbrasile@users.noreply.github.com>
2025-02-21 17:14:14 +0100

    still moving files

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: 'public',
+  root: 'public', // relative to the client folder
   plugins: [react()],
   build: {
-    // outDir is resolved relative to the root, so this places files at client/public/../dist,
-    // which simplifies to client/dist.
+    // outDir is relative to the current root (public), so '../dist' resolves to client/dist
     outDir: '../dist',
     emptyOutDir: true,
   }
